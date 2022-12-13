90944ec820ba3b5f8d6a02d99471bb135f98dc64
Abdiasiis halane <112101372+abdiasiis78@users.noreply.github.com>
2022-12-13 09:40:27 +0300

    wan sameyay

--- js/index.js
+++ js/index.js
@@ -4,10 +4,34 @@
 
 /* 
 
-1. Start creating three prompts, the first two for the two numbers and the second one for the operation
-2. Also declare another variable for the result.
-3. Using conditional statement, check if the operation is addition, subtraction, division or multipilcation
-4. Add the output to the result variable you created.
-5. Console.log the result.
+ Start creating three prompts, the first two for the two numbers and the second one for the operation
+ Also declare another variable for the result.
+Using conditional statement, check if the operation is addition, 
+subtraction, division or multipilcation
+ Add the output to the result variable you created.
+ Console.log the result.
 
 */
+
+// Declare the variables for the two numbers and the operation
+let num1 = 5;
+let num2 = 7;
+let operation = "+";
+
+// Declare a variable for the result
+let result;
+
+// Check the value of the operation variable and perform the corresponding operation
+if (operation === "+") {
+  result = num1 + num2;
+} else if (operation === "-") {
+  result = num1 - num2;
+} else if (operation === "*") {
+  result = num1 * num2;
+} else if (operation === "/") {
+  result = num1 / num2;
+}
+
+// Print the result to the console
+console.log(result);
+
